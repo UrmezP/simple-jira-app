2218935aa51b33f44ccee76a368d5a1a9c3e0f88
UrmezP <panthakiurmez@gmail.com>
2023-09-27 20:07:03 +0530

    added localstorage fn

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 class Project {
   static _counter = 0;
-  constructor(title, duration) {
+  constructor(title, duration, state = "NEW") {
     this._instanceCount = ++Project._counter;
     this._title = title;
     this._duration = duration;
+    this._state = state;
   }
 
   get projectId() {
@@ -17,14 +18,29 @@ class Project {
   get durationState() {
     return this._duration;
   }
+
+  get flowState() {
+    return this._state;
+  }
 }
 
-const listOfProjects = [];
+let listOfProjects = [];
 
 const newProjectButton = document.getElementById("newProject");
 const newProjectInputDialog = document.getElementById("newProjectDialog");
 const submitNewProject = document.querySelector("#newProjectDialog form");
 const projectFlow = document.querySelector("#projectFlow");
+
+// check if projects already present in localstorage
+
+if (localStorage.getItem("simple-jira-projects")) {
+  listOfProjects = JSON.parse(localStorage.getItem("simple-jira-projects"));
+
+  // add project to flow
+  listOfProjects.map((project) =>
+    addNewProject(new Project(project._title, project._duration))
+  );
+}
 
 function newProjectButtonClick() {
   newProjectInputDialog.showModal();
@@ -34,43 +50,11 @@ function drag(e) {
   e.dataTransfer.setData("text", e.target.id);
 }
 
-function updateProjectList() {
-  // remove New column div
-  projectFlow.querySelector("div:nth-of-type(1)").removeChild();
-
-  // create fresh New column
-  const newDiv = document.createElement("div");
-  const newHeading = document.createElement("h3");
-  newHeading.innerText = "New";
-  newDiv.append(newHeading);
-  projectFlow.prepend(newDiv);
-
-  // map over listOfProject array, create elements and appendChild to projectFlow element
-  listOfProjects.map((project) => {
-    const projectHTML = document.createElement("article");
-    const projectId = document.createElement("span");
-    const projectTitle = document.createElement("h3");
-    const projectDuration = document.createElement("span");
-
-    projectId.innerText = `Project #${project.projectId}`;
-    projectTitle.innerText = `Title: ${project.titleState}`;
-    projectDuration.innerText = `Duration - ${project.durationState} days`;
-    projectHTML.setAttribute("id", project.projectId);
-    projectHTML.appendChild(projectId);
-    projectHTML.appendChild(projectTitle);
-    projectHTML.appendChild(projectDuration);
-
-    // make it dragabble
-
-    projectHTML.setAttribute("draggable", "true");
-    projectHTML.addEventListener("dragstart", drag);
-
-    newDiv.appendChild(projectHTML);
-  });
-}
-
 function addNewProject(project) {
   const newDiv = projectFlow.querySelector("div:nth-of-type(1)");
+  const inProgressDiv = projectFlow.querySelector("div:nth-of-type(2)");
+  const completedDiv = projectFlow.querySelector("div:nth-of-type(3)");
+  const archivedDiv = projectFlow.querySelector("div:nth-of-type(4)");
 
   const projectHTML = document.createElement("article");
   const projectId = document.createElement("span");
@@ -90,7 +74,28 @@ function addNewProject(project) {
   projectHTML.setAttribute("draggable", "true");
   projectHTML.addEventListener("dragstart", drag);
 
-  newDiv.appendChild(projectHTML);
+  switch (project.flowState) {
+    case "NEW": {
+      newDiv.appendChild(projectHTML);
+      break;
+    }
+    case "INPROGRESS": {
+      inProgressDiv.appendChild(projectHTML);
+      break;
+    }
+    case "COMPLETED": {
+      completedDiv.appendChild(projectHTML);
+      break;
+    }
+    case "ARCHIVED": {
+      archivedDiv.appendChild(projectHTML);
+      break;
+    }
+    default: {
+      alert("ERROR");
+      break;
+    }
+  }
 }
 
 function submitNewProjectInputDialog(e) {
@@ -98,8 +103,11 @@ function submitNewProjectInputDialog(e) {
   const title = e.target[0].value;
   const duration = e.target[1].value;
   const freshProject = new Project(title, duration);
+
+  // update in localstorage
   listOfProjects.push(freshProject);
-  //   updateProjectList();
+  localStorage.setItem("simple-jira-projects", JSON.stringify(listOfProjects));
+
   addNewProject(freshProject);
   newProjectInputDialog.close();
 }
